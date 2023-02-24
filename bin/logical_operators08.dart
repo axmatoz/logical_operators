03ac0dc5b05497c5bda0e298eeacade659175cb8
axmatoz <axmatozodbek@gmail.com>
2023-02-24 08:51:53 +0500

    logical_operators08.dart

--- bin/logical_operators08.dart
+++ bin/logical_operators08.dart
@@ -8,5 +8,10 @@ Create a function argument  called a, b of type int
     Returns:
         bool: answer
 */
+bool func(int a, int b) {
+  return a % 2 == 0 || b % 2 == 0;
+}
 
-void main() {}
+void main() {
+  print(func(9, 5));
+}
